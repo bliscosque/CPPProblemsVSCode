173f72048b32729f3099af6cc629407cf786c58e
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-03-13 19:50:58 -0600

    fast power

---
+++ CourseUdemy/Recursion/fast_power.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//idea behind: a^n = a^((n/2)2), se n for par
+//                   a*a^((n/2)2), se n for impar
+//dessa maneira, o problema inicial eh dividido sempre ao meio
+
+int fast_power(int a , int n) {
+    if (n==0) return 1;
+    int subProblem=fast_power(a,n/2);
+    int subProblemSquare=subProblem*subProblem;
+    if (n&1) { //impar
+        return a*subProblemSquare;
+    }
+    return subProblemSquare; //par
+}
+
+int main()
+{
+    cout << fast_power(2,10) << endl;
+    return 0;
+}
