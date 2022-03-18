788d4222a052b560f051569d47b54302763b5ee7
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-03-18 07:52:33 -0600

    generate Brackets

--- CourseUdemy/Backtracking/letterCombinationPhone.cpp
+++ CourseUdemy/Backtracking/letterCombinationPhone.cpp
@@ -63,3 +63,40 @@ int main() {
     }
     return 0;
 }
+
+/* RESULTADO DO CURSO
+https://github.com/apaarkamal/Coding-Minutes-Competitive-Programming/blob/main/Coding%20Exercise%20Solutions/Letter%20Combinations%20of%20a%20Phone%20Number.cpp
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<string>mapping={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+void solve(int pos, string &cur, vector<string>&ans,string &digits)
+{
+    if(pos==n)
+    {
+        ans.push_back(cur);
+        return;
+    }
+    for(char  ch: mapping[digits[pos]-'2'])
+    {
+        cur.push_back(ch);
+        solve(pos+1,cur,ans,digits);
+        cur.pop_back();
+    }
+}
+vector<string> letterCombinations(string digits) {
+    vector<string>ans;
+    n=digits.size();
+    if(n==0)
+    {
+        return ans;
+    }
+    string cur;
+    solve(0,cur, ans,digits);
+    sort(ans.begin(),ans.end());
+    return ans;
+}
+
+*/
