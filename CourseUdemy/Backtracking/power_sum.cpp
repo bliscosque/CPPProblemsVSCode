8bd003a59f3d1eccd281ff311ce4e6bd81acd818
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-03-29 20:07:42 -0600

    power sum

--- CourseUdemy/Backtracking/power_sum.cpp
+++ CourseUdemy/Backtracking/power_sum.cpp
@@ -1,43 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ans=0;
 
 bool test(vector<int> &vi, int x, int n) {
     int sum=0;
-    for (auto i: vi) {
-        cout << "i: " << i << endl;
-        sum+=pow(i,n);
-    }
+
     cout << endl;
     return sum==x;
 }
 
-void generate(int x, int n, vector<int> &vi, int amt, int max, int last) {
-    if (vi.size() == amt) {
-        if (test(vi,x,n)) ans++;
-        return;
+int generate(int x, int n, vector<int> &vi) {
+    int sum=0;
+    int ans=0; 
+
+    for (auto i: vi) {
+        sum+=pow(i,n);
     }
-    for (int j=last;j<=max;j++) {
-        generate(x,n,vi,amt,max,last+1);
-        vi.push_back(j);
-        generate(x,n,vi,amt,max,last+1);
-        vi.pop_back();
+    if (sum==x) return 1;
+    else {
+        int answer=0;
+        int v=vi.empty()?1:vi.back()+1;
+        while (sum+pow(v,n) <= x) {
+            vi.push_back(v);
+            answer+=generate(x,n,vi);
+            vi.pop_back();
+            v++;
+        }
+        return answer;
     }
 }
 
 int solve(int x,int n){
     vector<int> vi;
-    int max=pow(x,1.0/n)+1;
-    //for (int i=1;i<=max;i++) {
-    //    generate(x,n,vi,i, max,1);
-    //}
-    generate(x,n,vi,1, max,1);
-    return ans;
+
+    return generate(x,n,vi);
+
 }
 
 int main() {
     cout << solve(13,2) << endl;
-    //cout << solve(100,2) << endl;
+    cout << solve(100,2) << endl;
     return 0;
 }
