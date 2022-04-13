d94a2a6e4797c9666afc0c4402925759d99d6c28
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-04-13 15:53:29 -0500

    started sort strings

---
+++ CourseUdemy_DS_Alg/strings/sort_strings.cpp
@@ -0,0 +1,14 @@
+//given a list of n strings
+//strings consists of digits and spaces
+//no. of spaces equal in n strings
+//variation of sort command
+//no space in begin, end, no consecutive spaces
+//key: column to be used as sorted
+//reversed: true or false
+//comparison-type: lexigraphical or numeric
+
+//logic:
+//1) extract keys from every string (tokenization) pair<string, key)
+//2) sorting, 2 comparators: lexicographical or numerical
+//3) reverse 
+
