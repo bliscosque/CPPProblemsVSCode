4778030cdcdfa3cd887c0d6fdf5d20eb7e9d0f6e
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-04-14 12:05:24 -0500

    biggest number string - author solution

--- CourseUdemy_DS_Alg/strings/biggest_number_string.cpp
+++ CourseUdemy_DS_Alg/strings/biggest_number_string.cpp
@@ -64,3 +64,29 @@ int main() {
     cout << concatenate(numbers2) << endl;
     return 0;
 }
+
+
+/* Solution of author
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(string s1,string s2){
+    return s1 + s2 > s2 + s1;
+}
+
+string concatenate(vector<int> numbers){
+    vector<string> output;
+
+    for(int no:numbers){
+        output.push_back(to_string(no));
+    }
+    sort(output.begin(),output.end(),compare);
+
+    string ans = "";
+    for(string x:output){
+        ans += x;
+    }
+    return ans;
+}
+
+*/
