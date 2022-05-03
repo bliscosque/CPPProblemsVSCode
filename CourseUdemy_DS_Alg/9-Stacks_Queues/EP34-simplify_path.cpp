307da6cd66cc4cc4c43f5ee91816a4adee200650
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-05-03 11:47:13 -0500

    simplify path

--- CourseUdemy_DS_Alg/9-Stacks_Queues/EP34-simplify_path.cpp
+++ CourseUdemy_DS_Alg/9-Stacks_Queues/EP34-simplify_path.cpp
@@ -1,12 +1,41 @@
+//working only for absolute paths
+
 #include<vector>
 #include<string>
 #include<sstream>
+#include <iostream>
+#include<deque>
 using namespace std;
 
 
 string simplifyPath(string path){
-   //complete this code return the simplified path
-   
-   
-   
+   istringstream iss(path);
+   vector<string> tokens;
+   deque<string> dq;
+   string token;
+
+   while (getline(iss,token,'/')) {
+       if (token=="" || token==".") continue;
+       else if (token=="..") {
+           if (!dq.empty()) {
+               dq.pop_back();
+           }
+       }
+       else {
+           dq.push_back(token);
+       }
+   }
+   string ans="";
+   for (auto str:dq) {
+       cout << "/" << str;
+        ans+="/"+str;
+   }   
+   return ans;
+}
+
+int main() {
+    string path="/../x/y/../z/././w/a///../../c/./";
+    simplifyPath(path);
+
+    return 0;
 }
