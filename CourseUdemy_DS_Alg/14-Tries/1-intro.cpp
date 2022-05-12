7e42a922970ad969d6127c42b183bc299663aa1b
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-05-11 22:04:50 -0500

    starting tries

---
+++ CourseUdemy_DS_Alg/14-Tries/1-intro.cpp
@@ -0,0 +1,19 @@
+//important DS for Pattern Matching Problems
+//given a list of strings and Q Que each query containing a word, you have to tell whether the word is contained in the list or not.
+
+//words={"apple", "ape", "mango", "news", "no"}
+//queries={"apple", "banana", "new", "mango"}
+
+//yes, no, no, yes
+
+//1-brute force
+//2-insert DS (e.g.: BST)
+//3-unorderedSet (hashing)
+//4-tries search: (O(w) -> tamanho da palavra) / Insert O(w)
+
+//what is a trie?
+//1- generic tree like DS
+//2- efficient retrieval DS
+//3- uses extra storage
+//operations: insert/search
+
