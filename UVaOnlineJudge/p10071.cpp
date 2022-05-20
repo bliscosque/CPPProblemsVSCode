2601edbc756f3c72034f3a4e10d47764ed4e1081
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-05-20 12:44:14 -0500

    started UVA

---
+++ UVaOnlineJudge/p10071.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Get-Content .\p10071.in |.\p10071.exe
+
+int main() {
+    int v, t;
+    while (scanf("%d %d\n",&v,&t)==2){
+        cout << 2*v*t << endl;
+    }
+
+    return 0;
+}
