8c681c2e6fbfdd52896766ab0ff5d67e87a7077b
Thiago Goncalves <bliscosque@yahoo.com.br>
2022-05-25 22:41:31 -0500

    continue 106

--- LeetCode/106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ LeetCode/106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -4,19 +4,20 @@
  * [106] Construct Binary Tree from Inorder and Postorder Traversal
  */
 
-//20222405 - 1
+//20220524 - 1
+//20220525 - 2
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
 
 // @lc code=start
 /**
@@ -32,25 +33,25 @@ struct TreeNode {
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int val=postorder.back();
-        postorder.pop_back();
-        TreeNode* root=new TreeNode(val);
+    TreeNode* buildTreeRec(vector<int>& inorder, int i1, int i2, vector<int>& postorder, int p1, int p2) {
+        int r=postorder[p2];
+        TreeNode* root=new TreeNode(r);
 
-        auto f_root=find(inorder.begin(),inorder.end(),val);
-
-        while (!postorder.empty()) {
-            val=postorder.back();
-            postorder.pop_back();
-
-            auto f=find(inorder.begin(),inorder.end(),val);
-            if (f>f_root) {
-                root->right=new TreeNode(val);
-            }
-            else {
-                root->left=new TreeNode(val);
-            }
+        int i;
+        for (i=i1;i<=i2;i++) {
+            if (inorder[i]==p2) break;
         }
+        int n_elem=i1-i;
+        if (n_elem>=1) {
+            root->left=buildTreeRec(inorder,i1,i-1,postorder,p1,p1+n_elem);
+            root->right=buildTreeRec(inorder,i+1,i,postorder,p1+n_elem,p2);
+        }
+        return root;
+
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n=inorder.size();
+        return buildTreeRec(inorder,0,n-1,postorder,0,n-1);
     }
 };
 // @lc code=end
