dde46fe28b123f8d7ef578c896961ff208e3260a
Thiago Goncalves <bliscosque@yahoo.com.br>
2022-05-28 20:01:56 -0500

    344

---
+++ LeetCode/344.reverse-string.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=344 lang=cpp
+ *
+ * [344] Reverse String
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+    }
+};
+// @lc code=end
+
