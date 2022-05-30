80f89b17bbcbc235d3e215e4349c7fa4ea5b83b2
Thiago Goncalves <bliscosque@yahoo.com.br>
2022-05-30 12:33:38 -0500

    162

--- LeetCode/162.find-peak-element.cpp
+++ LeetCode/162.find-peak-element.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem162.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode id=162 lang=cpp
  *
@@ -5,6 +13,7 @@
  */
 
 //20220529
+//20220530
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,14 +22,19 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+        if (nums.size()==1) return 0;
+        //agregando as regras para elemento 0 e ultimo
+        nums.insert(nums.begin(),INT_MIN);
+        nums.push_back(INT_MIN);
+
         int s=0, e=nums.size()-1;
         int mid;
         while (s<=e) {
             mid=s+(e-s)/2;
-            if (mid > 0 && mid<nums.size()-2 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) {
-                return mid;
+            if (mid > 0 && mid<nums.size()-1 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) {
+                return mid-1;
             }
-            else if (mid > 0 && nums[mid] > nums[mid-1]) {
+            else if (mid > 0 && nums[mid] >= nums[mid-1]) {
                 s=mid+1;
             }
             else {
