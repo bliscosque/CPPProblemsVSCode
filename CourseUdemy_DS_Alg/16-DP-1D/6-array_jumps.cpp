3337cabf31e158a239fb0636a0f69638d7e9e213
Thiago Goncalves <bliscosque@yahoo.com.br>
2022-05-31 11:52:52 -0500

    6

---
+++ CourseUdemy_DS_Alg/16-DP-1D/6-array_jumps.cpp
@@ -0,0 +1,11 @@
+//given an array of positive integers
+//each element -> max no of steps you can jump in array
+//find minimum jumps to reach to final idx
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr={3,4,2,1,2,3,7,1,1,1,2,5}; //4 (3->4->2->7->5)
+    return 0;   
+}
