36dfc6c65bce99649ed9a9f988cb293eec6da58e
Thiago Goncalves <bliscosque@yahoo.com.br>
2022-06-06 21:30:50 -0500

    70

--- LeetCode/70.climbing-stairs.cpp
+++ LeetCode/70.climbing-stairs.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem70.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode id=70 lang=cpp
  *
@@ -9,21 +17,23 @@
 // @lc code=start
 class Solution {
 public:
+    
     int ans=0;
-    void climbStairs(int n, int s) {
-        if (s>n) return;
-        if (s==n) {
-            ans++;
-            return;
-        }
-        climbStairs(n,s+1);
-        climbStairs(n,s+2);
+
+    int climbStairs(int n, vector<int> &dp) {
+        if (n<0) return 0; //passou
+        if (n==0) return 1; //chegou
+        if (dp[n]!=-1) return dp[n];
+        int ans=climbStairs(n-1,dp)+climbStairs(n-2,dp);
+        return dp[n]=ans;
+        
     }
     int climbStairs(int n) {
-        if (n<=1) return n;
-        ans=0;
-        climbStairs(n,0);
-        return ans;
+        vector<int> dp(50,-1);
+        dp[0]=0;
+        dp[1]=1;
+
+        return climbStairs(n-1,dp)+climbStairs(n-2,dp);
     }
 };
 // @lc code=end
