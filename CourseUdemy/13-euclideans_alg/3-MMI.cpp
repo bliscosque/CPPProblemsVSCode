3a979687740b5b7a285fce179bed1bae8dc25072
Thiago <bliscosque@yahoo.com.br>
2022-08-11 16:58:13 -0500

    mmi

--- CourseUdemy/13-euclideans_alg/3-MMI.cpp
+++ CourseUdemy/13-euclideans_alg/3-MMI.cpp
@@ -6,14 +6,11 @@ using namespace std;
 //retorno: {x,y}
 
 vector<int> extendedGCD(int a,int b){
-
 	if(b==0){
 		//return the values of x and y 
 		return {1,0,a};
 	}
 	vector<int> result = extendedGCD(b, a%b);
-
-	// After recursive call is over
 	int smallX = result[0];
 	int smallY = result[1]; 
 	int gcd  = result[2];
