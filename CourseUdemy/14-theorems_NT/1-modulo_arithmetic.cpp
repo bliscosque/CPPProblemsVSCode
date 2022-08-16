e6645e9bec9ddffde64a720eacd284c117d27b91
Thiago <bliscosque@yahoo.com.br>
2022-08-16 14:58:05 -0500

    Fermat little theorem

--- CourseUdemy/14-theorems_NT/1-modulo_arithmetic.cpp
+++ CourseUdemy/14-theorems_NT/1-modulo_arithmetic.cpp
@@ -15,10 +15,25 @@ int mulm(int x, int y) {
     return (x*y)%P; //o (...+P)%P serve para evitar resposta negativa
 }
 
+int powr(int x, int y) {
+    int res=1;
+    while(y) {
+        if (y & 1) res=mulm(res,x);
+        y/=2;
+        x=mulm(x,x);
+    }
+    return res;
+}
+
+int divm(int x, int y) {
+    return mulm(x,powr(y,P-2));
+}
+
 int32_t main() {
     cout << addm(3,5) << endl;
     cout << (-4%5) << endl; //em c++, isso retorna -4. Mas de acordo com T.N., o resto precisa estar entre [0,p[
     cout << subm(-3,5) << endl;
     cout << mulm(324,234) << endl;
+    cout << divm(56,2) << endl;
     return 0;
 }
