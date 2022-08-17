240c6613c73240fe08f3d6143b4c52646a3a47b4
Thiago <bliscosque@yahoo.com.br>
2022-08-17 12:12:30 -0500

    fact modulo

--- CourseUdemy/14-theorems_NT/1-modulo_arithmetic.cpp
+++ CourseUdemy/14-theorems_NT/1-modulo_arithmetic.cpp
@@ -2,7 +2,7 @@
 #define int long long int
 using namespace std;
 
-const int P=5;
+const int P=7, N=1e5;
 int addm(int x, int y) {
     return (x+y)%P;
 }
@@ -29,11 +29,22 @@ int divm(int x, int y) {
     return mulm(x,powr(y,P-2));
 }
 
+int fact[N];
+void calculate_factorial() {
+    fact[0]=1;
+    for (int i=1;i<N;i++) {
+        fact[i]=mulm(fact[i-1],i);
+    }
+}
+
 int32_t main() {
     cout << addm(3,5) << endl;
     cout << (-4%5) << endl; //em c++, isso retorna -4. Mas de acordo com T.N., o resto precisa estar entre [0,p[
     cout << subm(-3,5) << endl;
     cout << mulm(324,234) << endl;
     cout << divm(56,2) << endl;
+    
+    calculate_factorial(),
+    cout << fact[5];
     return 0;
 }
